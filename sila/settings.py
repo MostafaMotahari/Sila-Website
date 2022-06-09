353de516da7b3cc58b060112834c0638f3debc81
Mostafa Motahari <mostafamotahari2004@gmail.com>
2022-06-09 15:04:38 -0400

    Delete uneccesery JS files and update setting for static files.

--- sila/settings.py
+++ sila/settings.py
@@ -149,10 +149,7 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
