fccb2dee83ad9ef7b35f69a0258db969bfaabb45
Mostafa Motahari <mostafamotahari2004@gmail.com>
2022-06-09 12:17:13 -0400

    Set allowd host form env.

--- sila/settings.py
+++ sila/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ["sila-demo.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = [config('ALLOWED_HOST'), "127.0.0.1"]
 
 
 # Application definition
