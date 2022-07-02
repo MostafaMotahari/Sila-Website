8e0d97710a1075e4d64ada26aa5552d2dbf293a8
Mostafa Motahari <mostafamotahari2004@gmail.com>
2022-07-02 09:22:32 -0400

    Add showing top scorer in main page.

--- league/views.py
+++ league/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
+import league
+
+from league.models import (
+    League,
+    Team
+)
+from account.models import (
+    User
+)
 
 # Create your views here.
 def home(request):
-    return render(request, 'league/index.html')
+    # Finding top scorer of each league
+    users = User.objects.all().order_by("season_goals")
+    leagues = [league for league in League.objects.all()]
+    top_scorers = []
+
+    for user in users:
+        if not leagues:
+            break
+
+        if user.team.league in leagues:
+            top_scorers.append(user)
+            leagues.remove(user.team.league)
+
+    context = {
+        "top_scorers": top_scorers,
+    }
+    return render(request, 'league/index.html', context)
